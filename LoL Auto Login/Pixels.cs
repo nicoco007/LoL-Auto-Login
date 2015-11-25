08b0d29328b2a1eab0f4728888ee0c7b8c43fa70
nicoco007 <support@nicoco007.com>
2015-11-24 23:20:53 -0500

    Expanded color recognition range for Launch button

--- LoL Auto Login/Pixels.cs
+++ LoL Auto Login/Pixels.cs
@@ -6,9 +6,7 @@ namespace LoLAutoLogin
     class Pixels
     {
 
-        public static Pixel LaunchButtonEn = new Pixel(new Point(575, 15), Color.FromArgb(255, 199, 135, 12));
-        public static Pixel LaunchButtonFr = new Pixel(new Point(575, 15), Color.FromArgb(255, 201, 137, 13));
-        public static Pixel LaunchButton = new Pixel(new Point(575, 15), Color.FromArgb(255, 198, 134, 11), Color.FromArgb(255, 202, 138, 14));
+        public static Pixel LaunchButton = new Pixel(new Point(575, 15), Color.FromArgb(255, 190, 110, 10), Color.FromArgb(255, 210, 140, 30));
         public static Pixel PasswordBox = new Pixel(new PointF(0.192f, 0.480f), Color.FromArgb(255, 242, 242, 242), Color.FromArgb(255, 243, 243, 243), true);
 
     }
