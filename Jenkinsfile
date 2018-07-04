b6a947d36a7ed8c3084ddc5d1161674cdb5b1908
Nicolas Gnyra <nicolas@gnyra.com>
2018-07-03 21:40:53 -0400

    Update assembly build number

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,6 +9,7 @@ pipeline {
     stage('Build') {
       steps {
         bat 'nuget restore'
+        bat 'python update_build_number.py'
         bat 'msbuild /p:Configuration=Release /p:Platform=x86'
         bat 'iscc installer.iss'
         bat '7z a LoLAutoLogin-%GIT_BRANCH%-%BUILD_NUMBER%.zip "./LoL Auto Login/bin/x86/Release/*.dll" "./LoL Auto Login/bin/x86/Release/*.exe"'
---
+++ update_build_number.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+build = os.environ["BUILD_NUMBER"] if "BUILD_NUMBER" in os.environ else 0
+
+expr = re.compile(r"\[assembly: Assembly(\w*)Version\(\"(\d+)\.(\d+)\.(\d+)\.(\d+)\"\)\]", re.IGNORECASE)
+
+with open("LoL Auto Login\Properties\AssemblyInfo.cs", "r+", encoding="utf-8") as f:
+    read_lines = f.readlines()
+    write_lines = []
+
+    for line in read_lines:
+        write_lines.append(expr.sub(r'[assembly: Assembly\1Version("\2.\3.\4.{}")]'.format(build), line))
+        print(write_lines[-1])
+
+    f.seek(0)
+    f.truncate(0)
+
+    for line in write_lines:
+        f.write(line)
