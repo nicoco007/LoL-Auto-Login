05b06c2332d17fde44af10b4602dbdb475b69714
Nicolas Gnyra <nicolas@gnyra.com>
2018-07-08 19:22:06 -0400

    Improve build number script

--- update_build_number.py
+++ update_build_number.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-build = os.environ["BUILD_NUMBER"] if "BUILD_NUMBER" in os.environ else 0
+build = os.environ["BUILD_NUMBER"] if "BUILD_NUMBER" in os.environ else "0"
 
 expr = re.compile(r"\[assembly: Assembly(\w*)Version\(\"(\d+)\.(\d+)\.(\d+)\.(\d+)\"\)\]", re.IGNORECASE)
 
@@ -9,8 +9,13 @@ with open("LoL Auto Login\Properties\AssemblyInfo.cs", "r+", encoding="utf-8") a
     read_lines = f.readlines()
     write_lines = []
 
-    for line in read_lines:
-        write_lines.append(expr.sub(r'[assembly: Assembly\1Version("\2.\3.\4.{}")]'.format(build), line))
+    for i, line in enumerate(read_lines):
+        if (expr.search(line)):
+            new_line = expr.sub(r'[assembly: Assembly\1Version("\2.\3.\4.{}")]'.format(build), line)
+            print("{} -> {} on line {}".format(line.strip(), new_line.strip(), i + 1))
+            write_lines.append(new_line)
+        else:
+            write_lines.append(line)
 
     f.seek(0)
     f.truncate(0)
